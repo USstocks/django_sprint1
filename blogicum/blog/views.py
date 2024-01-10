92ecc673ea8166bc3b7ee63070de3860bc6da9b8
Масленников Денис <usstocksnews@yandex.ru>
2024-01-10 18:00:25 +0300

    Infinite try

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -59,5 +59,5 @@ def post_detail(request, pk):
 
 
 def category_posts(request, category_slug):
-    return render(request, 'blog/category.html', 
+    return render(request, 'blog/category.html',
                   {'category_slug': category_slug})
